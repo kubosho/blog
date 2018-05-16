f761f30ae01c7a2916c32b3bf559616ebaca4b6d
kubosho <ta2@o2p.jp>
2018-05-17 01:04:18 +0900

    chore: add postcss-custom-media

--- themes/wkr/postcss.config.js
+++ themes/wkr/postcss.config.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const postCSSimport = require('postcss-import');
+const customMedia = require('postcss-custom-media');
 
 module.exports = {
   plugins: [
     postCSSimport,
+    customMedia,
   ],
 };
