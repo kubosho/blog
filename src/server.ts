d904b4ea252fe6be25abd0cc644c7f39c59aa4d7
Shota Kubota <ta2@o2p.jp>
2019-01-05 20:47:51 +0900

    feat(src): add port constants in server

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import * as express from 'express';
 import { createContentfulClient } from './entriesGateway';
 import { route } from './routing';
 
+const PORT = process.env.PORT || 8080;
+
 function main() {
   dotenvConfig();
 
@@ -10,7 +12,10 @@ function main() {
   const client = createContentfulClient(process.env.SPACE, process.env.ACCESS_TOKEN);
 
   route(app, client);
-  app.listen(8080);
+  app.listen(PORT);
+
+  // tslint:disable-next-line no-console
+  console.log(`The server is running at port:${PORT}`);
 }
 
 main();
