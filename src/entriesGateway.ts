b94f74aaecd98848ba9b31607d0bd8ae227c59c8
Shota Kubota <ta2@o2p.jp>
2019-01-07 02:36:30 +0900

    feat(src): add created excerpt text function

--- src/entriesGateway.ts
+++ src/entriesGateway.ts
@@ -1,7 +1,20 @@
+import { unwrapOrFromUndefinable } from 'option-t/lib/Undefinable/unwrapOr';
 import { ContentfulClientApi, EntryCollection, createClient } from 'contentful';
 import * as marked from 'marked';
 
 import { EntryPlainObject, EntryValue } from './entryValue';
+
+function createExcerptText(fields: EntryPlainObject): string {
+  const excerpt = fields.excerpt;
+  const contentExcerpt = stripParagraphElement(marked(fields.content.split('\n')[0]));
+
+  const r = unwrapOrFromUndefinable(excerpt, contentExcerpt);
+  return r;
+}
+
+function stripParagraphElement(content: string): string {
+  return content.replace(/<\/?p>/g, '');
+}
 
 export function createContentfulClient(space: string, accessToken: string): ContentfulClientApi {
   const c = createClient({
@@ -16,10 +29,13 @@ export async function fetchEntries(client: ContentfulClientApi): Promise<EntryVa
   const entries: EntryCollection<EntryPlainObject> = await client.getEntries();
   const result = entries.items.map(item => {
     const content = marked(item.fields.content);
+    const excerpt = createExcerptText(item.fields);
+
     const o = {
       ...item.sys,
       ...item.fields,
       content,
+      excerpt,
     };
     const ev = new EntryValue(o);
     return ev;
