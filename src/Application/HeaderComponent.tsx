b734f6933f8fc26674e3280b2bda13d277915664
Shota Kubota <ta2@o2p.jp>
2019-01-06 19:28:07 +0900

    feat(assets): add site title style

--- src/Application/HeaderComponent.tsx
+++ src/Application/HeaderComponent.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { SITE_TITLE } from './constants';
 
 export const HeaderComponent = (): JSX.Element => (
-  <header>
-    <h1>{SITE_TITLE}</h1>
+  <header className="com-Application-header">
+    <h1 className="com-Application-header__site-title">{SITE_TITLE}</h1>
   </header>
 );
