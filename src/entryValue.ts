71ea6e89ac2b20cd51050e185e8db0e8df92eb59
Shota Kubota <ta2@o2p.jp>
2019-01-07 10:43:14 +0900

    feat(src): required excerpt value

--- src/entryValue.ts
+++ src/entryValue.ts
@@ -4,10 +4,10 @@ import { orForMaybe } from 'option-t/lib/Maybe/or';
 export interface EntryPlainObject {
   content: string;
   createdAt: string;
+  excerpt: string;
   id: string;
   title: string;
   updatedAt: string;
-  excerpt?: string;
   publishedAt?: string;
   slug?: string;
 }
@@ -15,20 +15,20 @@ export interface EntryPlainObject {
 export class EntryValue {
   readonly content: string;
   readonly createdAt: string;
+  readonly excerpt: string;
   readonly id: string;
   readonly title: string;
   readonly updatedAt: string;
-  readonly excerpt?: string;
   readonly publishedAt?: string;
   readonly slug?: string;
 
   constructor(param: EntryPlainObject) {
     this.content = unwrapMaybe(param.content);
     this.createdAt = unwrapMaybe(param.createdAt);
+    this.excerpt = unwrapMaybe(param.excerpt);
     this.id = unwrapMaybe(param.id);
     this.title = unwrapMaybe(param.title);
     this.updatedAt = unwrapMaybe(param.updatedAt);
-    this.excerpt = orForMaybe(param.excerpt, null);
     this.publishedAt = orForMaybe(param.updatedAt, null);
     this.slug = orForMaybe(param.slug, null);
   }
