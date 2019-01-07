0c6803cd01d3baf4548f7d921e6caea829d85439
Shota Kubota <ta2@o2p.jp>
2019-01-07 23:52:56 +0900

    feat(routing): add entry page routing

--- src/routing.ts
+++ src/routing.ts
@@ -1,12 +1,18 @@
 import { ContentfulClientApi } from 'contentful';
 import * as express from 'express';
 import { Nullable } from 'option-t/lib/Nullable/Nullable';
+import { Undefinable, isUndefined } from 'option-t/lib/Undefinable/Undefinable';
 
 import { EntryValue } from './entryValue';
 import { SITE_TITLE } from './Application/constants';
+import { EntryViewString } from './Entry/EntryViewString';
 import { TopViewString } from './Top/TopViewString';
 import { View } from './View';
 import { fetchEntries } from './entriesGateway';
+
+interface EntryPageParams {
+  slug: string;
+}
 
 export async function route(app: express.Express, client: ContentfulClientApi) {
   app.use('/assets', express.static(`${__dirname}/assets`));
@@ -27,5 +33,21 @@ export async function route(app: express.Express, client: ContentfulClientApi) {
         title: SITE_TITLE,
       }),
     );
+  });
+
+  app.get('/entry/:slug', (req, res) => {
+    const params: EntryPageParams = req.params;
+    const entry: Undefinable<EntryValue> = entries.find(e => e.slug === params.slug);
+
+    const body = EntryViewString(entry);
+
+    if (!isUndefined(entry)) {
+      res.send(
+        View({
+          body,
+          title: `${entry.title} · ${SITE_TITLE}`,
+        }),
+      );
+    }
   });
 }
