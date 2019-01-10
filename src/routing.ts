36a11ba6eae502bfcefe998d3cf72dd855547410
Shota Kubota <ta2@o2p.jp>
2019-01-11 02:15:36 +0900

    feat(src): use Top & Entry route handlers in routing

--- src/routing.ts
+++ src/routing.ts
@@ -1,43 +1,13 @@
 import * as express from 'express';
-import { Undefinable, isUndefined } from 'option-t/lib/Undefinable/Undefinable';
 
+import { ASSETS_PATH } from './Application/paths';
+import { entryRouteHander } from './Entry/EntryRouteHandler';
+import { topRouteHander } from './Top/TopRouteHandler';
 import { EntryValue } from './entryValue';
-import { SITE_TITLE } from './Application/constants';
-import { EntryViewString } from './Entry/EntryViewString';
-import { TopViewString } from './Top/TopViewString';
-import { View } from './View';
-
-interface EntryPageParams {
-  slug: string;
-}
 
 export async function route(app: express.Express, entries: EntryValue[]) {
-  app.use('/assets', express.static(`${__dirname}/assets`));
+  app.use(ASSETS_PATH, express.static(`${__dirname}${ASSETS_PATH}`));
 
-  app.get('/', (req, res) => {
-    const body = TopViewString(entries);
-
-    res.send(
-      View({
-        body,
-        title: SITE_TITLE,
-      }),
-    );
-  });
-
-  app.get('/entry/:slug', (req, res) => {
-    const params: EntryPageParams = req.params;
-    const entry: Undefinable<EntryValue> = entries.find(e => e.slug === params.slug);
-
-    const body = EntryViewString(entry);
-
-    if (!isUndefined(entry)) {
-      res.send(
-        View({
-          body,
-          title: `${entry.title} · ${SITE_TITLE}`,
-        }),
-      );
-    }
-  });
+  topRouteHander(app, entries);
+  entryRouteHander(app, entries);
 }
