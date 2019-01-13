be01277353d7cf545d93595c403cfaf5e34def69
Shota Kubota <ta2@o2p.jp>
2019-01-13 10:11:27 +0900

    feat(src): required slug

--- src/entryValue.ts
+++ src/entryValue.ts
@@ -1,15 +1,14 @@
 import { unwrapMaybe } from 'option-t/lib/Maybe/unwrap';
-import { orForMaybe } from 'option-t/lib/Maybe/or';
 
 export interface EntryPlainObject {
   content: string;
   createdAt: string;
   excerpt: string;
   id: string;
+  slug: string;
   title: string;
   updatedAt: string;
   publishedAt?: string;
-  slug?: string;
 }
 
 export class EntryValue {
@@ -17,17 +16,17 @@ export class EntryValue {
   readonly createdAt: string;
   readonly excerpt: string;
   readonly id: string;
+  readonly slug: string;
   readonly title: string;
   readonly updatedAt: string;
-  readonly slug?: string;
 
   constructor(param: EntryPlainObject) {
     this.content = unwrapMaybe(param.content);
     this.createdAt = unwrapMaybe(param.createdAt);
     this.excerpt = unwrapMaybe(param.excerpt);
     this.id = unwrapMaybe(param.id);
+    this.slug = unwrapMaybe(param.slug);
     this.title = unwrapMaybe(param.title);
     this.updatedAt = unwrapMaybe(param.updatedAt);
-    this.slug = orForMaybe(param.slug, null);
   }
 }
