574f256d60cd873cc82f0bf59338de9e6bbaeff5
Shota Kubota <ta2@o2p.jp>
2019-01-14 22:15:04 +0900

    feat(Application): add top page link in title component

--- src/Application/HeaderComponent.tsx
+++ src/Application/HeaderComponent.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import { SITE_TITLE } from './constants';
+import { TOP_PATH } from './paths';
 
 export const HeaderComponent = (): JSX.Element => (
   <header className="com-Application-header">
-    <h1 className="com-Application-header__site-title">{SITE_TITLE}</h1>
+    <h1 className="com-Application-header__site-title">
+      <a href={TOP_PATH}>{SITE_TITLE}</a>
+    </h1>
   </header>
 );
