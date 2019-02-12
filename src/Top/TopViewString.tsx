b40d74b32181e6d298c415e3186a391ea4d1149b
Shota Kubota <ta2@o2p.jp>
2019-02-12 11:09:13 +0900

    feat(src): remove needless Viewstring files

--- src/Top/TopViewString.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { renderToString } from 'react-dom/server';
-
-import { EntryValue } from '../entryValue';
-import { TopComponent } from './TopComponent';
-
-export function TopViewString(entries: EntryValue[]): string {
-  return renderToString(<TopComponent entries={entries} />);
-}
