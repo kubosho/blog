c577770f6a32f808e8bf7cf0435c1e78d41c3741
Shota Kubota <ta2@o2p.jp>
2019-03-06 13:59:23 +0900

    feat(src): add mine type list enum

---
+++ src/mineTypes.ts
@@ -0,0 +1,12 @@
+export enum MINE_TYPES {
+  HTML = 'text/html',
+  MJS = 'application/javascript',
+  JS = 'text/javascript',
+  JSON = 'application/json',
+  CSS = 'text/css',
+  JPEG = 'image/jpeg',
+  GIF = 'image/gif',
+  PNG = 'image/png',
+  WEBP = 'image/webp',
+  SVG = 'image/svg+xml',
+}
