33fcb33dc0fbd37a2f66472af54a523359d1786d
Shota Kubota <ta2@o2p.jp>
2019-03-25 21:16:37 +0900

    test(Page/Entry): add EntryComponent tests

---
+++ src/Page/Entry/__tests__/test_EntryComponent.jsx
@@ -0,0 +1,25 @@
+import test from 'ava';
+import React from 'react';
+import ShallowRenderer from 'react-test-renderer/shallow';
+import { EntryValue } from '../../../Entry/entryValue';
+import { EntryComponent } from '../EntryComponent';
+
+test('EntryComponent', t => {
+  const renderer = new ShallowRenderer();
+  const mockEntryPlainObject = {
+    content: 'test content',
+    createdAt: '2016-12-20T10:43:35.772Z',
+    excerpt: 'test content',
+    id: '5KsDBWseXY6QegucYAoacS',
+    slug: 'hello-world',
+    title: 'Hello, world!',
+    updatedAt: '2016-12-20T10:43:35.772Z',
+  };
+  const mockEntryValue = new EntryValue(mockEntryPlainObject);
+  const component = <EntryComponent entry={mockEntryValue} />;
+
+  renderer.render(component);
+
+  const r = renderer.getRenderOutput();
+  t.snapshot(r);
+});
