6310b5f168638b67f93eea608dfb4472ed0c0110
Shota Kubota <ta2@o2p.jp>
2019-04-29 19:59:02 +0900

    feat(Page/Common): add dispalyed publish date component

---
+++ src/Page/Common/components/PublishedDate.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { formatYYMMDDString, parseISOFormat } from '../../../Application/date';
+
+type Props = {
+  createdAt: string;
+};
+
+export const PublishedDate = (props: Props) => {
+  const { createdAt } = props;
+  const timeValue = formatYYMMDDString(parseISOFormat(createdAt));
+  return <time dateTime={createdAt}>{timeValue}</time>;
+};
