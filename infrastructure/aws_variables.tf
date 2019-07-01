fd860da8b12759ddf90c92e22fa48b318da2669a
Shota Kubota <ta2@o2p.jp>
2019-07-01 12:47:59 +0900

    chore(terraform): add terraform configs

---
+++ infrastructure/aws_variables.tf
@@ -0,0 +1,7 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
+variable "app_name" {}
+variable "root_segment" {}
+variable "public_segment1_az" {}
+variable "public_segment2_az" {}
